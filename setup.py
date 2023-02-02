fad2f893b40d94d8e2f197a9feb9b0a2fb453e73
DedInc <visitanimation@gmail.com>
2023-02-02 11:39:52 +0600

    v1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='byerecaptcha',
-    version='1.2.3',
+    version='1.2.4',
     author='Maehdakvan',
     author_email='visitanimation@google.com',
     description='Google Recaptcha solver with selenium.',
